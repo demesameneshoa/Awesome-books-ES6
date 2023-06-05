ddeeeb565887f2af11ff53fa5d66375996e41137
demesameneshoa <demesameneshoa@gmail.com>
2023-06-05 23:25:09 +0300

    modify readme file, ignore luxon.js from linters

--- JS/modules/displaydate.js
+++ JS/modules/displaydate.js
@@ -1,4 +1,5 @@
 // display time
+import { DateTime } from './luxon.js';
 
 const doDate = () => {
   const currentDate = document.querySelector('.date');
@@ -6,15 +7,15 @@ const doDate = () => {
     'August', 'September', 'October', 'November', 'December',
   ];
   const suffixes = ['th', 'st', 'nd', 'rd'];
-  const date = new Date();
-  const month = months[date.getMonth()];
-  const day = date.getDate();
+  const date = DateTime.now();
+  const month = months[date.month];
+  const { day } = date;
   const suffix = day % 10 <= 3 ? suffixes[day % 10] : suffixes[0];
-  const hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-  const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-  const dateString = `${month} ${day}${suffix} ${date.getFullYear()}, ${hour}:${minutes}:${seconds} ${ampm}`;
+  const hour = date.hour > 12 ? date.hour - 12 : date.hour;
+  const ampm = date.hour >= 12 ? 'PM' : 'AM';
+  const minutes = date.minute.toString().padStart(2, '0');
+  const seconds = date.second.toString().padStart(2, '0');
+  const dateString = `${month} ${day}${suffix} ${date.year}, ${hour}:${minutes}:${seconds} ${ampm}`;
   currentDate.innerHTML = `${dateString}`;
 };
 
